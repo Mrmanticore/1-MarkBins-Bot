7b0433157feccca7c2bfe2dd8cd0fd0ec4d87657
Markbin-team <adityaprojects404@gmail.com>
2024-04-09 18:25:26 +0530

    image fix

--- api/mark.py
+++ api/mark.py
@@ -6,6 +6,7 @@ import base64
 import smtplib
 import ssl
 from email.message import EmailMessage
+import io
 
 app = Flask(__name__)
 
@@ -36,12 +37,15 @@ def tweet():
     image_data = data.get('image')  # Base64-encoded image data
 
     # Decode the base64 image data and save it to a file
+    base64_bytes = image_data.encode('ascii')
+    image_bytes = base64.b64decode(base64_bytes)
     with open('temp_image.jpg', 'wb') as f:
-        f.write(base64.b64decode(image_data))
+        f.write(image_bytes)
 
     # Upload the image
-    media = clientv1.simple_upload(filename='temp_image.jpg')
-    media_id = media.media_id
+    with open('temp_image.jpg', 'rb') as image_file:
+        media = clientv1.simple_upload(filename='temp_image.jpg', file=image_file)
+        media_id = media.media_id
 
     # Tweet the message with the image
     response = clientv2.create_tweet(
@@ -57,7 +61,7 @@ def tweet():
     email_sender = os.getenv('EMAIL_SENDER')
     email_password = os.getenv('EMAIL_PASSWORD')
     email_receiver = email
-    subject = 'Tweet sent successfully , MarkBin team'
+    subject = 'Tweet sent successfully, MarkBin team'
     body = f"Thank you for your contribution! Your tweet has been successfully sent and is now live on Twitter. This is a great initiative towards cleaning and we appreciate your effort. Together, we can make a difference. Keep up the good work! Click here to view your tweet: https://twitter.com/user/status/{response.data['id']}\n\nMarkBin team regards"
     
     em = EmailMessage()
@@ -72,8 +76,10 @@ def tweet():
         smtp.sendmail(email_sender, email_receiver, em.as_string())
 
     return jsonify({'message': 'Tweet sent successfully!!'})
+
 @app.route('/')
 def index():
     return 'Flask Server is running successfully! -markbins'
+
 if __name__ == '__main__':
     app.run(debug=True)
